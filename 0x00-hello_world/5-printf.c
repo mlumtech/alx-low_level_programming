0582f69680276e07fa2d3d694db5e04a93f62b02
root <mlumtech>
2022-09-11 13:12:32 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in main function
+ *
+ * Description: using the main function
+ * This programe prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0
+ */
+ int main(void)
+ {
+	 prinf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+}
+
